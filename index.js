36e5388003ca4856b61b2bbc32296bc449d951e1
Andrey Bispo <andreybispof@gmail.com>
2024-06-04 22:03:46 -0300

    Iniciando Repositório

---
+++ index.js
@@ -0,0 +1,10 @@
+let nomeHeroi="João";
+let xpHeroi;
+let nivelHeroi="Prata";
+
+console.ReadLine(nomeHeroi)
+console.ReadLine(xpHeroi)
+
+
+
+console.log("O Herói de nome " + nomeHeroi " está no nível de " + nivelHeroi);
