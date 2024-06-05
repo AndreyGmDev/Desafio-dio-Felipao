ac165e685949f4044a99a1ebc544e848c44b9888
Andrey Bispo <andreybispof@gmail.com>
2024-06-05 17:04:36 -0300

    Código do Desafio proposto

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-let nomeHeroi="João";
-let xpHeroi;
-let nivelHeroi="Prata";
+let nomeHeroi="João"
+let xpHeroi=10001
+let nivelHeroi
+if(xpHeroi<1001)
+{
+   nivelHeroi="Ferro"
+}
+if(xpHeroi>=1001 && xpHeroi<2001)
+{
+  nivelHeroi="Bronze"
+}
+if(xpHeroi>=2001 && xpHeroi<5001)
+{
+  nivelHeroi="Prata"
+}
+if(xpHeroi>=5001 && xpHeroi<7001)
+{
+  nivelHeroi="Ouro"
+}
+if(xpHeroi>=7001 && xpHeroi<8001)
+{
+  nivelHeroi="Platina"
+}
+if(xpHeroi>=8001 && xpHeroi<9001)
+{
+  nivelHeroi="Ascendente"
+}
+if(xpHeroi>=9001 && xpHeroi<10001)
+{
+  nivelHeroi="Imortal"
+}
+if(xpHeroi>=10001)
+{
+  nivelHeroi="Radiante"
+}
 
-console.ReadLine(nomeHeroi)
-console.ReadLine(xpHeroi)
-
-
-
-console.log("O Herói de nome " + nomeHeroi " está no nível de " + nivelHeroi);
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
